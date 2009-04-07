7eefbbd1d127bf70be29c94c26b6668d5f2d04df
Ramin Rakhamimov <ramin32@gmail.com>
2009-04-07 12:57:06 -0400

    Finished up three_cnf_sat_finder. Need to fix range of large inputs.

--- three_cnf_sat_finder.py
+++ three_cnf_sat_finder.py
@@ -23,56 +23,59 @@ negate = lambda b: not b
 """Returns the identify of the input."""
 identify = lambda b: b
 
-def generate_random_operator_list():
+def generate_cnf():
     """Generates a 3-cnf list."""
     operators = (negate, identify)
-    return tuple([random.choice(operators) for i in xrange(3)])
+    return [random.choice(operators) for i in xrange(3)]
 
 def create_random_3_cnf_expression(size):
     """Creates a list of 3-cnf lists of given size."""
-    expression = []
-    for i in xrange(size):
-        expression.append(generate_random_operator_list())
+    return [generate_cnf() for i in xrange(size)]
 
-    return tuple(expression)
+def create_boolean_groups(x, max_size):
+    """Constructs binary groups of size 3 taken from splitting 
+    the binary string representation of x."""
+    raw_binary = bin(x)[2:].zfill(max_size)                    # strip the 0b header and pad with 0's
+    booleans = [{'0': False, '1':True}[b] for b in raw_binary] # map 0's and 1's to booleans
+    binary_groups = group_split(booleans, 3) 
+    return binary_groups
 
-def padded_binary(x, max_size):
-    """Returns x as a binary string padded with 0's upto max_size."""
-    binary = bin(x)[2:]
-    padding_left = max_size - len(binary)
-    if padding_left > 0:
-        binary = ''.join((''.join(['0' for i in xrange(padding_left)]), binary))
-    return binary
-            
-def evaluate_cnf(cnf_exp, binary_string):
+
+def group_split(seq, size):
+    """Splits the given sequence into groups of size."""
+    return [seq[i:i+size] for i in range(0, len(seq), size)]
+
+
+def evaluate_cnf_with_booleans(cnf_exp, boolean_groups):
     """Plugs in the binary string into the cnf expression evaluating to true or false."""
-    pass
-
-
+    zipped = zip(cnf_exp, boolean_groups)
+    evaluate_cnf = lambda z: any([z[0][i](z[1][i]) for i in xrange(3)])
+    cnf_evaluations = map(evaluate_cnf, zipped)
+    return all(cnf_evaluations)
 
 def solve_cnf(cnf_exp):
     """Returns first 3-cnf-sat match using bruteforce."""
     size = len(cnf_exp) * 3
     for i in xrange(2**size):
-        binary_string = padded_binary(i, size)
-        if evaluate_cnf(cnf_exp, binary_string):
-            return binary_string
+        boolean_groups = create_boolean_groups(i, size)
+        if evaluate_cnf_with_booleans(cnf_exp, boolean_groups):
+            return boolean_groups
     return None
 
 
-def operator_str(operator):
+def operator_str(operator, param='X'):
     """Returns a pretty string of the function."""
     if operator == identify:
-       return 'identify'
+       return 'identify(%s)' % param 
     elif operator == negate:
-        return 'negate' 
+        return 'negate(%s)' % param 
 
     raise ValueError('input %s not negate or identify' % str(operator))
 
 def pretty_print(cnf_exp):
     """Pretty prints a cnf expression."""
     create_cnf_string = lambda cnf: " V ".join(map(operator_str, cnf))
-    parentecize = lambda inner_part: "".join(('(',inner_part ,' )'))
+    parentecize = lambda inner_part: "".join(('(',inner_part ,')'))
 
     print " ^ ".join([parentecize(create_cnf_string(cnf)) for cnf in cnf_exp])
 
@@ -87,6 +90,7 @@ def main():
     solution = solve_cnf(generated_cnf_expression)
     print 'Solution:'
     print solution
+        
 
 
 
